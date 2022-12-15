f1bcf4873ced0e176602cfd6fc83c769a558b964
Vineesh Chauhan <vineesh.chauhan@ukg.com>
2022-12-15 17:17:59 +0530

    Add negative breakout table

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
 function productList() {
+  console.log('productList');
     // Call Web API to get a list of Product
     $.ajax({
       url: 'https://script.googleusercontent.com/macros/echo?user_content_key=61RnUtQJUeWTNrbiKlOkWbDher7R1PMAqtFapFxZQEmtWoRKwGayGDPpAKqfJH27uZAMcl9hf2r1SqlqFOxqAQH6eRHLaelTm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnMfISo38izSjt4QVmgjONp1yNjMTi4Xyv2lsTqW_x8kaYVxysINYZOiezBNcMGFQceIENn0XDjw6qydn_OX-LMZZr3eOZigE6tz9Jw9Md8uu&lib=M-3rGwz2vKgMkWX0ph3dLJW_yCJ7QuvMt',
       type: 'GET',
       dataType: 'json',
       success: function (products) {
-        productListSuccess(products);
+        productListSuccess(products,$("#positivemomentum tbody"));
+      },
+      error: function (request, message, error) {
+        handleException(request, message, error);
+      }
+    });
+    productListNegative();
+  }
+
+  function productListNegative() {
+    console.log('productListNegative');
+    // Call Web API to get a list of Product
+    $.ajax({
+      url: 'https://script.googleusercontent.com/macros/echo?user_content_key=-yQ87-B7a2_2XpMeXGtK-EWxyT5a1l6tgWnfMUQag1b6l1rySzbNRolvFVWs1MEE6HvKFsQ43TZ5eetIPhE28EgSaZKfJysSm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnCcBv6YuEVTyf1op_0nNDVV7O70bD6Pvs4wTj45WEKDTwBI1_Ni2ExO5RtXPhRHkQKrvg35aL2l7KUxzuWWZKTGx2nJX4aRLm9z9Jw9Md8uu&lib=M-3rGwz2vKgMkWX0ph3dLJW_yCJ7QuvMt',
+      type: 'GET',
+      dataType: 'json',
+      success: function (products) {
+        productListSuccess(products,$("#negativemomentum tbody"));
       },
       error: function (request, message, error) {
         handleException(request, message, error);
@@ -13,22 +31,19 @@ function productList() {
     });
   }
 
-  function productListSuccess(products) {
+  function productListSuccess(products,element) {
     // Iterate over the collection of data
     $.each(products.data, function (index, product) {
       // Add a row to the Product table
-      productAddRow(product);
+      productAddRow(product,element);
     });
   }
 
-  function productAddRow(product) {
+  function productAddRow(product,element) {
     // Check if <tbody> tag exists, add one if not
-     if ($("#positivemomentum tbody").length == 0) {
-      $("#positivemomentum").append("<tbody></tbody>");
-     }
      // Append row to <table>
-     $("#positivemomentum tbody").append(
-       productBuildTableRow(product));
+     $(element.append(
+       productBuildTableRow(product)));
    }
 
    function productBuildTableRow(product) {
